ae1502babd197bd48873a7883735f749d07d8b8c
Carlos Eduardo Lourenco <engenharia.lourenco@gmail.com>
2020-03-29 14:28:50 -0300

    Simple example using xUnit

---
+++ CShp_Calculation/CShp_Calculation/Calculation.cs
@@ -0,0 +1,30 @@
+﻿namespace CShp_Calculation
+{
+    public static class Calculation
+    {
+        public static double Sum(double number1, double number2)
+        {
+            return (number1 + number2);
+        }
+
+        public static double Subtract(double number1, double number2)
+        {
+            return (number1 - number2);
+        }
+
+        public static double Multiply(double number1, double number2)
+        {
+            return (number1 * number2);
+        }
+
+        public static double Divide(double number1, double number2)
+        {
+            return (number1 / number2);
+        }
+
+        public static bool IsEvenNumber(int number)
+        {
+            return number % 2 == 0;
+        }
+    }
+}
---
+++ CShp_Calculation/CShp_CalculationTest/CalculationUnitTest.cs
@@ -0,0 +1,81 @@
+﻿using CShp_Calculation;
+using Xunit;
+
+namespace CShp_CalculationTest
+{
+    public class CalculationUnitTest
+    {
+        [Fact]
+        public void Sum_TwoDouble_ReturnDouble()
+        {
+            // Arrange
+            var num1 = 2.9;
+            var num2 = 3.1;
+            var expectedValue = 6;
+
+            // Act
+            var soma = Calculation.Sum(num1, num2);
+
+            // Assert
+            Assert.Equal(expectedValue, soma);
+        }
+
+        [Fact]
+        public void Subtract_TwoDouble_ReturnDouble()
+        {
+            // Arrange
+            var num1 = 2.9;
+            var num2 = 3.1;
+            var expectedValue = -0.2;
+
+            // Act  
+            var subtraction = Calculation.Subtract(num1, num2);
+
+            //Assert  
+            Assert.Equal(expectedValue, subtraction, 1);
+        }
+
+        [Fact]
+        public void Mutiply_TwoDouble_ReturnDouble()
+        {
+            // Arrange  
+            var num1 = 2.9;
+            var num2 = 3.1;
+            var expectedValue = 8.99;
+
+            // Act
+            var mult = Calculation.Multiply(num1, num2);
+
+            // Assert
+            Assert.Equal(expectedValue, mult, 2);
+        }
+
+        [Fact]
+        public void Divide_TwoDouble_ReturnDouble()
+        {
+            // Arrange  
+            var num1 = 2.9;
+            var num2 = 3.1;
+            var expectedValue = 0.94; // Rounded value 
+            
+            // Act  
+            var div = Calculation.Divide(num1, num2);
+
+            //Assert  
+            Assert.Equal(expectedValue, div, 2);
+        }
+
+        [Theory]
+        [InlineData(1, 2, 3)]
+        [InlineData(-4, -6, -10)]
+        [InlineData(-2, 2, 0)]
+        [InlineData(int.MinValue, 1, int.MinValue+1)]
+        public void Sum_TwoRelativeNumbers_ReturnRelativeNumber(int num1, int num2, int expectedValue)
+        {
+            //Act
+            var result = Calculation.Sum(num1, num2);
+            //Assert
+            Assert.Equal(expectedValue, result);
+        }
+    }
+}
