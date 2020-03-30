d981a17cad2223231ff14dce020011b3b1a7c367
Carlos Eduardo Lourenco <engenharia.lourenco@gmail.com>
2020-03-30 18:18:29 -0300

    Another simple example using NUnit

---
+++ VirtualStore/VirtualStore.Domain/Entities/Item.cs
@@ -0,0 +1,21 @@
+﻿namespace VirtualStore.Domain.Entities
+{
+    public class Item
+    {
+        public string Description { get; private set; }
+        public int Amount { get; set; }
+        public double UnitValue { get; set; }
+
+        public Item(string description, int amount, double unitValue)
+        {
+            Description = description;
+            Amount = amount;
+            UnitValue = unitValue;
+        }
+
+        public double TotalValue
+        {
+            get { return UnitValue * Amount; }
+        }
+    }
+}
---
+++ VirtualStore/VirtualStore.Domain/Entities/ShoppingCart.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+namespace VirtualStore.Domain.Entities
+{
+    public class ShoppingCart
+    {
+        public IList<Item> Items { get; private set; }
+
+        public ShoppingCart()
+        {
+            Items = new List<Item>();
+        }
+
+        public void Add(Item item)
+        {
+            Items.Add(item);
+        }
+
+        public double MaxValue()
+        {
+            if (Items.Count == 0) return 0;
+
+            double major = Items[0].TotalValue;
+            foreach (var item in Items)
+            {
+                if (major < item.TotalValue)
+                {
+                    major = item.TotalValue;
+                }
+            }
+
+            return major;
+        }
+    }
+}
