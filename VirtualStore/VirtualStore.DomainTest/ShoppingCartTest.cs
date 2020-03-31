26b6cc0f089aa821a3afbc9a07ecb4b7b1e3647d
Carlos Eduardo Lourenco <engenharia.lourenco@gmail.com>
2020-03-31 11:46:14 -0300

    Test class improvmnets

---
+++ VirtualStore/VirtualStore.DomainTest/ShoppingCartTest.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+using VirtualStore.Domain.Entities;
+
+namespace VirtualStore.DomainTest
+{
+    [TestFixture]
+    class ShoppingCartTest
+    {
+        private ShoppingCart _cart;
+
+        [SetUp]
+        public void Initialize()
+        {
+            _cart = new ShoppingCart();
+        }
+
+        [Test]
+        public void ShouldReturnZeroIfEmptyCart()
+        {
+            Assert.AreEqual(0.0, _cart.MaxValue(), 0.0001);
+        }
+
+        [Test]
+        public void ShouldReturnValueOfCartForOneItem()
+        {
+            _cart.Add(new Item("Refrigerator", 1, 900.0));
+            Assert.AreEqual(900.0, _cart.MaxValue(), 0.0001);
+        }
+
+        [Test]
+        public void ShouldReturnMaxValueForManyItems()
+        {
+            _cart.Add(new Item("Refrigerator", 1, 900.0));
+            _cart.Add(new Item("Stove", 1, 1500.0));
+            _cart.Add(new Item("Washer", 1, 750.0));
+
+            Assert.AreEqual(1500.0, _cart.MaxValue(), 0.0001);
+        }
+    }
+}
