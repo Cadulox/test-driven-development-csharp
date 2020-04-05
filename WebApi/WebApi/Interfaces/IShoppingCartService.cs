2da2cb0da6cd3fce180fe2c500ca37d778a9f1f9
Carlos Eduardo Lourenco <engenharia.lourenco@gmail.com>
2020-04-05 14:44:24 -0300

    Unit Testing in ASP.NET Core Web API using xUnit

---
+++ WebApi/WebApi/Interfaces/IShoppingCartService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using WebApi.Models;
+
+namespace WebApi.Interfaces
+{
+    public interface IShoppingCartService
+    {
+        IEnumerable<ShoppingItem> GetAllItems();
+        ShoppingItem Add(ShoppingItem newItem);
+        ShoppingItem GetById(Guid id);
+        void Remove(Guid id);
+    }
+}
---
+++ WebApi/WebApi/Models/ShoppingCartService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using WebApi.Interfaces;
+
+namespace WebApi.Models
+{
+    public class ShoppingCartService : IShoppingCartService
+    {
+        public ShoppingItem Add(ShoppingItem newItem)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<ShoppingItem> GetAllItems()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ShoppingItem GetById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
